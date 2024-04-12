c3e5904bee8cb40470916fe8e53dd6c00da5f5c8
Joy-Juyoung <juyu9407@gmail.com>
2024-04-11 22:41:57 -0600

    Add: Temporary header

---
+++ src/Page.js
@@ -0,0 +1,15 @@
+import Header from './Components/Header/Header';
+
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      <header>
+        <Header />
+      </header>
+    </div>
+  );
+};
+
+export default page;
