eb60438b7c644e2fbf8fa6ccc3a9fff0c5614974
Joy-Juyoung <juyu9407@gmail.com>
2024-04-11 23:02:10 -0600

    Modified: logo

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -27,14 +27,14 @@ const SideWrap = {
 };
 
 const logoImg = {
-  // height: '30px',
+  // height: '40px',
   height: '50px',
 };
 
 const logoTitle = {
   height: '80px',
   margin: '0px 10px',
-  fontSize: '28px',
+  fontSize: '24px',
 
   display: 'flex',
   alignItems: 'center',
@@ -46,7 +46,10 @@ const Header = () => {
       <div style={wrapper}>
         <div style={SideWrap}>
           <img style={logoImg} src={Logo} alt='logo' />
-          <h1 style={logoTitle}>MINIMATE</h1>
+          <h1 style={logoTitle}>MINI-MATE</h1>
+        </div>
+        <div style={SideWrap}>
+          <h1 style={logoTitle}>Search</h1>
         </div>
         <div style={SideWrap}>
           <h1 style={logoTitle}>Right side</h1>
