5be1af48c406315b59512acf503872e84c3c3ecf
Joy-Juyoung <juyu9407@gmail.com>
2024-04-16 08:21:50 -0600

    Added button to open new window popup

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import { Header } from '../components';
+import { Buttons, Header } from '../components';
+import { AiOutlineHome } from 'react-icons/ai';
 
 const Home = () => {
+  const openPopup = () => {
+    // Define the URL and window features for the popup
+    const popupUrl = 'http://localhost:3000/minihome';
+    const popupFeatures = 'width=600,height=400';
+
+    // Open the popup window
+    window.open(popupUrl, '_blank', popupFeatures);
+  };
+
   return (
     <div className='w-full bg-bgColor h-screen overflow-hidden'>
       <Header />
+      <div className='text-ascent-1 '>Click to Open window popup</div>
+      <button
+        onClick={openPopup}
+        className='flex items-center gap-2 text-sm text-white font-semibold px-4 md:px-6 py-1 md:py-2 bg-[#F37125] rounded-lg hover:bg-[#f4823f] shadow-lg'
+      >
+        <div className='text-xl'>
+          <AiOutlineHome />
+        </div>
+        Go to Minihome
+      </button>
     </div>
   );
 };
