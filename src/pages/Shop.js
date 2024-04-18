8bbe418f55141ce19c38cb65c7882ca56f4972bf
Joy-Juyoung <juyu9407@gmail.com>
2024-04-17 23:52:12 -0600

    Added minihome card in home page

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -4,24 +4,16 @@ import BgPattern from '../assets/pattern8.png';
 const Shop = () => {
   return (
     // <div className='w-full h-screen bg-primary overflow-hidden p-0 m-0'>
-    <div class='waveSection'>
-      <div class='wave'></div>
-    </div>
+    <>
+      <div class='waveSection'>
+        <div class='wave'></div>
+      </div>
+      <div className='w-full h-[55%] bg-primary px-24 py-6 z-10 mt-[10rem]'>
+        shop
+      </div>
+    </>
     // </div>
   );
 };
 
 export default Shop;
-
-{
-  /* <div
-    className='w-full h-[33.2rem] px-24 py-6 blur-[2px]'
-    style={{
-      backgroundImage: `url('${BgPattern}')`,
-      backgroundSize: '15%',
-    }}
-  ></div> */
-}
-{
-  /* <div className='h-[50vh]'>Shop</div> */
-}
