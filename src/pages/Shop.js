ad6690c37f8106f3bbbe8e18438493a8866d010d
Joy-Juyoung <juyu9407@gmail.com>
2024-04-18 00:00:31 -0600

    cleanup on shop page2

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -4,9 +4,7 @@ import BgPattern from '../assets/pattern8.png';
 const Shop = () => {
   return (
     <div className='w-full h-screen bg-primary overflow-hidden p-0 m-0'>
-      <div className='w-full h-[55%] bg-primary px-24 py-6 z-10 mt-[10rem]'>
-        shop
-      </div>
+      <div className='w-full bg-primary'>shop</div>
     </div>
   );
 };
