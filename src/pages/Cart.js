dff4a0ee8b407be2402af8e500cf911de2f91586
Joy-Juyoung <juyu9407@gmail.com>
2024-05-14 10:53:27 -0600

    fixed cart error and modified design

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,30 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Image1 from '../assets/minime(2).gif';
 import Image2 from '../assets/miniroom1.gif';
 import Image3 from '../assets/minime(10).gif';
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
 import { fetchAllCartsByUser, fetchCartItems } from '../redux/cartSlice';
 import { Buttons } from '../components';
 
 const Cart = ({ me }) => {
   const dispatch = useDispatch();
   const { item, list } = useSelector((state) => state.cart);
-
-  // console.log('me', me?._id);
-
+  const [cartItems, setCartItems] = useState(item?.shop_items || []);
   useEffect(() => {
     dispatch(fetchAllCartsByUser({ userId: me?._id }));
-    dispatch(fetchCartItems({ cartId: list[0]?._id }));
-  }, [dispatch]);
-  console.log('item', item);
+  }, [dispatch, me]);
 
-  const [cartItems, setCartItems] = useState(item?.shop_items || null);
-  console.log('list', list[0]);
+  useEffect(() => {
+    if (list && list.length > 0) {
+      dispatch(fetchCartItems({ cartId: list[0]._id }));
+    }
+  }, [dispatch, list]);
 
-  const [selectedItems, setSelectedItems] = useState([
-    ...Array(cartItems?.length).keys(),
-  ]); // 모든 아이템을 선택된 상태로 시작
+  // console.log(cartItems);
+
+  const [selectedItems, setSelectedItems] = useState(
+    Array.isArray(cartItems) ? [...Array(cartItems.length).keys()] : []
+  );
 
   const handleCheckboxChange = (index) => {
     if (selectedItems.includes(index)) {
@@ -39,49 +39,49 @@ const Cart = ({ me }) => {
   };
 
   const handleSelectAll = () => {
-    if (selectedItems.length === cartItems?.length) {
+    if (selectedItems.length === cartItems.length) {
       setSelectedItems([]);
     } else {
-      setSelectedItems([...Array(cartItems?.length).keys()]);
+      setSelectedItems([...Array(cartItems.length).keys()]);
     }
   };
 
   const handleDeleteSelected = () => {
-    const updatedCartItems = cartItems?.filter(
+    const updatedCartItems = cartItems.filter(
       (_, index) => !selectedItems.includes(index)
     );
     setCartItems(updatedCartItems);
     setSelectedItems([]);
   };
 
-  const totalPrice = selectedItems?.reduce(
-    (acc, index) => acc + cartItems[index]?.item_price,
+  const totalPrice = selectedItems.reduce(
+    (acc, index) => acc + cartItems[index].item_price,
     0
   );
   const totalItems = selectedItems.length;
-  const availablePoints = me?.point;
+  const availablePoints = me?.point || 0;
 
   return (
     <div className='w-full h-[100vh] flex flex-col py-16 px-10 2xl:px-40'>
       <div className='text-3xl font-bold mb-4'>Cart</div>
       <div className='flex justify-between items-center mb-4'>
-        <div>
+        <div className='flex items-center '>
           <input
             type='checkbox'
             checked={selectedItems.length === cartItems?.length}
             onChange={handleSelectAll}
             className='mr-2'
           />
-          <label className=''>Select All</label>
+          <label className='text-[0.8rem]'>Select All</label>
         </div>
         <Buttons
           onClick={handleDeleteSelected}
-          className='px-4 py-2 rounded hover:bg-[#bbb] bg-[#ddd]'
+          containerStyles='text-[0.8rem] px-4 py-2 rounded hover:bg-[#bbb] bg-[#ddd]'
           title='Delete Selected'
         />
       </div>
-      <div className='flex flex-col'>
-        {cartItems?.map((item, index) => (
+      <div className='flex flex-col '>
+        {cartItems.map((item, index) => (
           <div
             key={item.id}
             className='border-b border-[#aaa] py-3 flex items-center gap-8 '
@@ -107,19 +107,35 @@ const Cart = ({ me }) => {
           </div>
         ))}
       </div>
-      <div className='mt-8'>
-        <h2 className='text-xl font-semibold'>Payment Information</h2>
-        <p>Items Qty: {totalItems}</p>
-        <p>Total Price: ${formatPrice(totalPrice)}</p>
-        <p>Available Points: ${formatPrice(availablePoints)}</p>
-        <p>Remaining Balance: ${formatPrice(availablePoints - totalPrice)}</p>
-      </div>
+      <div className='w-full flex flex-col items-end my-8 rounded-md text-[0.9rem]'>
+        <div className='w-1/3 flex flex-col items-end bg-white p-4'>
+          <div className='w-full text-lg font-semibold mb-4'>
+            Payment Information
+          </div>
+          <div className='w-full flex items-center justify-between'>
+            <p>Items Qty:</p>
+            <p>{totalItems}</p>
+          </div>
+          <div className='w-full flex items-center justify-between'>
+            <p>Total Price:</p>
+            <p>${formatPrice(totalPrice)}</p>
+          </div>
+          <div className='w-full flex items-center justify-between'>
+            <p>Available Points:</p>
+            <p>${formatPrice(availablePoints)}</p>
+          </div>
+          <div className='w-full flex items-center justify-between'>
+            <p>Remaining Balance:</p>
+            <p>${formatPrice(availablePoints - totalPrice)}</p>
+          </div>
+        </div>
 
-      <div className='my-4 flex gap-4'>
-        <Buttons
-          className='px-4 py-2 rounded hover:bg-blue-600 ml-2'
-          title='Checkout'
-        />
+        <div className='my-4 flex gap-4'>
+          <Buttons
+            containerStyles='text-[0.8rem] px-4 py-2 rounded bg-black text-white '
+            title='Checkout'
+          />
+        </div>
       </div>
     </div>
   );
